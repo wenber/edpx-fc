b2dbc25a241f7ff15fb989c9d54c0bc1e57609ce
wenber <wenbo.fe@gmail.com>
2015-03-09 13:02:13 +0800

    增加自动打开浏览器功能

--- cli/fc/start.js
+++ cli/fc/start.js
@@ -17,6 +17,8 @@ cli.main = function (args, opts) {
     // 直接调用edp webserver
     var port = opts.port || 8848;
     spawn('edp', ['webserver', 'start', '--port=' + port]);
+    require('../../lib/fc/start').openBrowser(port);
 };
 
+/* global exports */
 exports.cli = cli;
